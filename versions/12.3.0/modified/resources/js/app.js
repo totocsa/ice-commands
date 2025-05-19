1d7947d2f7a0452ef064ea9f4ac418e658d94ff2
Csaba Tóth <totocsa@gmail.com>
2025-05-19 18:43:19 +0200

    Do not publish ice-icsesd-resources

--- versions/12.3.0/modified/resources/js/app.js
+++ versions/12.3.0/modified/resources/js/app.js
@@ -5,18 +5,37 @@ import "./Components/totocsa/axiosInterceptors.js"
 import { createApp, h } from "vue"
 import { createPinia } from "pinia"
 import { createInertiaApp } from "@inertiajs/vue3"
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers"
 import { ZiggyVue } from "../../vendor/tightenco/ziggy"
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel"
 
+const nameToPath = name => {
+    const vendorStart = name.indexOf("vendor/")
+    let path = name.substring(vendorStart - 1)
+    path = path.replace(/Pages\/.*$/, "")
+
+    return path
+}
+
+const nameResolve = name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    let res = pages[`./Pages/${name}.vue`]
+
+    if (res === undefined) {
+        let path = nameToPath(name)
+        if (path === '/vendor/totocsa/ice-users-gui/resources/js/') {
+            const users = import.meta.glob('/vendor/totocsa/ice-users-gui/resources/js/**/*.vue', { eager: true })
+            const index = name.substring(name.indexOf("/vendor/")) + '.vue'
+            res = users[index]
+        }
+    }
+
+    return res
+}
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob("./Pages/**/*.vue")
-        ),
+    resolve: name => nameResolve(name),
     setup({ el, App, props, plugin }) {
         const pinia = createPinia()
 
