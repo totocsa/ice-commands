6b798c939c8322de51ef8806d008f36448533b11
Csaba Tóth <totocsa@gmail.com>
2025-05-21 13:43:04 +0200

    Mysql and MariaDB

--- Console/Commands/SetEnv.php
+++ Console/Commands/SetEnv.php
@@ -7,20 +7,26 @@ use Illuminate\Console\Command;
 class SetEnv extends Command
 {
     protected $signature = 'ice:set-env'
-        . ' {--replace-file=~/.config/laravel/ice/defaults/env.replaces.php}'
+        . ' {--file=*}'
         . ' {--vite_port=5173}';
     protected $description = 'Set variables in .env file';
-    protected $replaceFile;
-    protected $defaults;
+    protected $files;
 
     public function handle()
     {
-        $this->replaceFile = $this->option('replace-file');
+        $this->files = $this->option('file');
         $errors = $this->validateOptions();
 
         if (count($errors) === 0) {
             $dirArray = explode(DIRECTORY_SEPARATOR, base_path());
             $dir = array_pop($dirArray);
+
+            $replaces = [];
+            foreach ($this->files as $v) {
+                $file = $this->replaceTilde($v);
+                $replaces = array_merge($replaces, require $file);
+            }
+
             $trFromTo = [
                 '{{dir}}' => $dir,
                 '{{vite_port}}' => preg_match('/^ice[0-9]{1,3}$/', $dir) ?
@@ -28,35 +34,50 @@ class SetEnv extends Command
                     $this->option('vite_port'),
             ];
 
-            $this->defaults = include $this->replaceFile;
-
-            foreach ($this->defaults as $v) {
+            foreach ($replaces as $v) {
                 $patterns[] = $v['pattern'];
                 $replacements[] = $v['replacement'];
             }
 
-            $content = file_get_contents(base_path('.env'));
+            //$content = file_get_contents(base_path('.env'));
+            $content = file_get_contents(base_path('.env.17477139899984'));
 
-            $newContent = str_replace("{{dir}}", $dir, preg_replace($patterns, $replacements, $content));
             $newContent = strtr(preg_replace($patterns, $replacements, $content), $trFromTo);
-
-            copy('.env', '.env.' . str_replace('.', '', microtime(true)));
-            file_put_contents('.env', $newContent);
+            file_put_contents('.env.0', $newContent);
+            //copy('.env', '.env.' . str_replace('.', '', microtime(true)));
+            //file_put_contents('.env', $newContent);
+        } else {
+            foreach ($errors as $field => $items) {
+                $this->error("$field error:");
+                foreach ($items as $item) {
+                    $msg = is_array($item) ? $item['message'] : $item;
+                    $this->line("  $msg");
+                }
+            }
         }
     }
 
     protected function validateOptions()
     {
         $errors = [];
-        if (substr($this->replaceFile, 0, 2) === '~' . DIRECTORY_SEPARATOR) {
-            $homepath = $_SERVER['HOME'] ?? (/*windows compatibility: */$_SERVER['USERPROFILE']);
-            $this->replaceFile = $homepath . substr($this->replaceFile, 1);
-        }
 
-        if (!file_exists($this->replaceFile)) {
-            $errors[] = "Defaults file not found: $this->replaceFile";
+        foreach ($this->files as $v) {
+            $file = $this->replaceTilde($v);
+            if (!is_file($file)) {
+                $errors['file'][] = "File not found: $file";
+            }
         }
 
         return $errors;
+    }
+
+    protected function replaceTilde($text)
+    {
+        if (substr($text, 0, 2) === '~' . DIRECTORY_SEPARATOR) {
+            $homepath = $_SERVER['HOME'] ?? (/*windows compatibility: */$_SERVER['USERPROFILE']);
+            return $homepath . substr($text, 1);
+        } else {
+            return $text;
+        }
     }
 }
